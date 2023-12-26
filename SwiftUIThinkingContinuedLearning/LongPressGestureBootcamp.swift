669b87f6fe84ae0d5f874ed58466aa347b375969
Yessimkhan <200103408@stu.sdu.edu.kz>
2023-12-26 19:02:02 +0600

    Initial Commit

---
+++ SwiftUIThinkingContinuedLearning/LongPressGestureBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  LongPressGestureBootcamp.swift
+//  SwiftUIThinkingContinuedLearning
+//
+//  Created by Yessimkhan Zhumash on 26.12.2023.
+//
+
+import SwiftUI
+
+struct LongPressGestureBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LongPressGestureBootcamp()
+}
