46a8f9276d8cbbc2bca1ed5e0d255d8ff8ad9966
Yessimkhan <200103408@stu.sdu.edu.kz>
2023-12-26 19:27:45 +0600

    added magnification gesture

---
+++ SwiftUIThinkingContinuedLearning/MagnificationGestureBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  MagnificationGestureBootcamp.swift
+//  SwiftUIThinkingContinuedLearning
+//
+//  Created by Yessimkhan Zhumash on 26.12.2023.
+//
+
+import SwiftUI
+
+struct MagnificationGestureBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MagnificationGestureBootcamp()
+}
