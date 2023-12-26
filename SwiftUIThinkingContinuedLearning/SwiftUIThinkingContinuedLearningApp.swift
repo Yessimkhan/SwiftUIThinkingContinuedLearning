63a71eebebe88f002ea0f10d29b27de934344e44
Yessimkhan <200103408@stu.sdu.edu.kz>
2023-12-26 19:02:02 +0600

    Initial Commit

---
+++ SwiftUIThinkingContinuedLearning/SwiftUIThinkingContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThinkingContinuedLearningApp.swift
+//  SwiftUIThinkingContinuedLearning
+//
+//  Created by Yessimkhan Zhumash on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThinkingContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
