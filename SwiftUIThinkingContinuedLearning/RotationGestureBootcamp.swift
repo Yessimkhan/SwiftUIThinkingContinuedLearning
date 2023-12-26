ed55d4bf535e1b0243ca23659557b6e9d65932f9
Yessimkhan <200103408@stu.sdu.edu.kz>
2023-12-26 19:30:13 +0600

    added rotation gesture

---
+++ SwiftUIThinkingContinuedLearning/RotationGestureBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  RotationGestureBootcamp.swift
+//  SwiftUIThinkingContinuedLearning
+//
+//  Created by Yessimkhan Zhumash on 26.12.2023.
+//
+
+import SwiftUI
+
+struct RotationGestureBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotationGestureBootcamp()
+}
