7016665971fb8c57d9745ca85edfa839d0960b56
Yessimkhan <200103408@stu.sdu.edu.kz>
2023-12-26 19:23:51 +0600

    added long tap gesture

--- SwiftUIThinkingContinuedLearning/LongPressGestureBootcamp.swift
+++ SwiftUIThinkingContinuedLearning/LongPressGestureBootcamp.swift
@@ -8,9 +8,81 @@
 import SwiftUI
 
 struct LongPressGestureBootcamp: View {
+    
+    @State var isComplete: Bool = false
+    @State var isSuccess: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Rectangle()
+                .fill(isSuccess ? Color.green : Color.blue)
+                .frame(maxWidth: isComplete ? .infinity : 0)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(.gray)
+            
+            HStack {
+                Text("Click here")
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.black)
+                    .cornerRadius(10)
+                    .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) { isPressing in
+                        // start of pressing -> min duration
+                        if isPressing {
+                            withAnimation(.easeInOut(duration: 1.0)) {
+                                isComplete = true
+                            }
+                        }
+                        else {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                if !isSuccess {
+                                    withAnimation(.easeInOut) {
+                                        isComplete = false
+                                    }
+                                }
+                            }
+                        }
+                    } perform: {
+                        // at the min duration
+                        withAnimation(.easeInOut) {
+                            isSuccess = true
+                        }
+                    }
+
+                
+                Text("Reset")
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.black)
+                    .cornerRadius(10)
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            isSuccess = false
+                            isComplete = false
+                        }
+                    }
+                    
+            }
+            
+        }
     }
+    
+    
+    
+    
+    
+//    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//            .padding()
+//            .padding(.horizontal)
+//            .background(isComplete ? .green : .gray)
+//            .onLongPressGesture(minimumDuration: 1, maximumDistance: 50) {
+//
+//                isComplete.toggle()
+//            }
+//            .cornerRadius(10)
+//    }
 }
 
 #Preview {
