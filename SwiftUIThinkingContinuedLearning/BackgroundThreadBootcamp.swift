ee93f1a4ddea5109fab53b4c4da4bac432f7b807
Yessimkhan <200103408@stu.sdu.edu.kz>
2024-01-29 14:22:17 +0600

    added Multi-threading with background threads and queues

---
+++ SwiftUIThinkingContinuedLearning/BackgroundThreadBootcamp.swift
@@ -0,0 +1,65 @@
+//
+//  BackgroundThreadBootcamp.swift
+//  SwiftUIThinkingContinuedLearning
+//
+//  Created by Yessimkhan Zhumash on 29.01.2024.
+//
+
+import SwiftUI
+
+class BackgroundThreadViewModel: ObservableObject {
+    @Published var dataArray: [String] = []
+    
+    func fetchData() {
+        DispatchQueue.global(qos: .background).async {
+            let newData = self.downloadData()
+            
+            print("Check 1: \(Thread.isMainThread)")
+            print("Check 1: \(Thread.current)")
+            
+            DispatchQueue.main.async {
+                self.dataArray = newData
+                print("Check 2: \(Thread.isMainThread)")
+                print("Check 2: \(Thread.current)")
+            }
+        }
+    }
+    
+    func downloadData() -> [String] {
+        var data: [String] = []
+        
+        for x in 0..<100 {
+            data.append("\(x)")
+            print(data)
+        }
+        return data
+    }
+}
+
+struct BackgroundThreadBootcamp: View {
+    
+    @StateObject var vm = BackgroundThreadViewModel()
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                Text("Load Data")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .onTapGesture {
+                        vm.fetchData()
+                    }
+                
+                ForEach(vm.dataArray, id: \.self) { item in
+                    Text(item)
+                        .font(.headline)
+                        .foregroundStyle(Color.red)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    BackgroundThreadBootcamp()
+}
--- SwiftUIThinkingContinuedLearning/SwiftUIThinkingContinuedLearningApp.swift
+++ SwiftUIThinkingContinuedLearning/SwiftUIThinkingContinuedLearningApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIThinkingContinuedLearningApp: App {
     var body: some Scene {
         WindowGroup {
-            CoreDataRelationshipsBootcamp()
+            BackgroundThreadBootcamp()
         }
     }
 }
